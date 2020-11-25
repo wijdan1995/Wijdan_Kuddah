b964ad10ee6dcac3e9f33ff3dc98c0941f824109
wijdan1995 <wijdan.w.k@outlook.sa>
2020-11-25 10:35:15 +0300

    change to functional components

--- mern/react/prop-it-up/src/PersonCard.js
+++ mern/react/prop-it-up/src/PersonCard.js
@@ -1,23 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age: props.age
-        };
-    }
-    render() {
-        const { firstName, lastName, age, hairColor } = this.props;
-        return (
-            <div class="card mb-2">
-                <div class="card-body">
-                <h4 class="card-title">{lastName}, {firstName}</h4>
-                <p class="card-text">Age: {this.state.age}</p>
-                <p class="card-text">Hair Color: {hairColor}</p>
-                <button type="button" class="btn btn-primary btn-block" onClick={()=> this.setState({age: parseInt(this.state.age)+1})}>Birthday Button for {firstName} {lastName}</button>
-                </div>
+const PersonCard = props => {
+    const { firstName, lastName, age, hairColor } = props;
+    return(
+        <div class="card mb-2">
+            <div class="card-body">
+            <h4 class="card-title">{lastName}, {firstName}</h4>
+            <p class="card-text">Age: {age}</p>
+            <p class="card-text">Hair Color: {hairColor}</p>
             </div>
-        )
-    }
+        </div>
+    );
 }
+export default PersonCard;
